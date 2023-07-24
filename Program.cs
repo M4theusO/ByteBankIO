14271fcd78c198cf6263d430e7781bd32ecc548c
Matheus de Oliveira <matheus_oliveira8@estudante.sc.senai.br>
2023-07-24 15:49:54 -0300

    Convertendo texto para conta

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿partial class Program
+﻿using ByteBankIO;
+
+partial class Program
 {
     static void Main(string[] args)
     {
@@ -15,11 +17,35 @@
             while(!leitor.EndOfStream)
             {
                 var linha = leitor.ReadLine();
-                Console.WriteLine(linha);
+                var contaCorrente = ConverterStringParaContaCorrente(linha);
+
+                var msg = $"Conta número {contaCorrente.Numero}, ag. {contaCorrente.Agencia}, Saldo: {contaCorrente.Saldo}";
+                Console.WriteLine(msg);
             }
-
         }
-
        Console.ReadLine();
+    }
+
+    static ContaCorrente ConverterStringParaContaCorrente(string linha)
+    {
+        // 123,4678,789.10,Gustavo Santos
+        var campos = linha.Split(',');
+        var agencia = campos[0];
+        var numero = campos[1];
+        var saldo = campos[2].Replace('.', ',');
+        var nomeTitular = campos[3];
+
+        var agenciaComoInt = int.Parse(agencia);
+        var numeroComoInt = int.Parse(numero);
+        var saldoComoDouble = double.Parse(saldo);
+
+        var titular = new Cliente();
+        titular.Nome = nomeTitular;
+
+        var resultado = new ContaCorrente(agenciaComoInt, numeroComoInt);
+        resultado.Depositar(saldoComoDouble);
+        resultado.Titular = titular;
+
+        return resultado;
     }
 }
