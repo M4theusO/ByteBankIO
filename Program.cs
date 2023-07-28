80f36b984d28106ed1a70b3723d72510ac866564
Matheus de Oliveira <matheus_oliveira8@estudante.sc.senai.br>
2023-07-28 15:07:44 -0300

    Auxiliares da classe File

--- Program.cs
+++ Program.cs
@@ -4,10 +4,18 @@ partial class Program
 {
     static void Main(string[] args)
     {
-        EscritaBinaria();  
-        LeituraBinaria();
+        var linhas = File.ReadAllLines("contas.txt"); //Lê todas as linhas do arquivo e retorna um array de strings
+        Console.WriteLine(linhas.Length);
 
+        /*foreach(var linha in linhas)
+        {
+            Console.WriteLine(linha);
+        }*/
+
+        var bytesArquivo = File.ReadAllBytes("contas.txt");
+        Console.WriteLine($"Arquivo contas.txt possui {bytesArquivo.Length} bytes");
+        File.WriteAllText("escrevendoComAClasseFile.txt", "Testando File.WriteAllText"); //Sobrescreve o arquivo
         Console.WriteLine("Aplicação finalizada. . .");
+        Console.ReadLine();
     }
-
 }
